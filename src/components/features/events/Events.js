51f331a95816bb70529c00f9781b0b9d4b5dee77
jwestcott9 <jwestcott9@gmail.com>
2025-03-29 20:45:53 -0600

    past events

--- src/components/features/events/Events.js
+++ src/components/features/events/Events.js
@@ -21,7 +21,27 @@ export const events = [
         description: "Our monthly curated showcase of local artists, producers, and DJs.",
         location: "The Lounge",
         flyer: `../images/event-flyers/mar22lounge.png`,
-        ticketLink: "https://blackboxdenver.co/events/wildflower-arts-mar22"
+    } , {
+        name: "Grow Room Monthly Showcase",
+        date: "2025-02-15",
+        time: "9:00 PM - 1:00 AM",
+        description: "Our monthly curated showcase of local artists, producers, and DJs.",
+        location: "The Lounge",
+        flyer: `../images/event-flyers/feb15thlounge.png`,
+    } , {
+        name: "Grow Room Monthly Showcase",
+        date: "2025-01-25",
+        time: "9:00 PM - 1:00 AM",
+        description: "Our monthly curated showcase of local artists, producers, and DJs.",
+        location: "The Lounge",
+        flyer: `../images/event-flyers/jan25lounge.png`,
+    },{
+        name: "Grow Room Monthly Showcase",
+        date: "2024-12-28",
+        time: "9:00 PM - 1:00 AM",
+        description: "Our monthly curated showcase of local artists, producers, and DJs.",
+        location: "The Lounge",
+        flyer: `../images/event-flyers/dec28lounge.jpg`,
     }
 ];
 
